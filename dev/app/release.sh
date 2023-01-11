78389b725fb5f8a007d36137b18268212756bfee
Quinn Slack <quinn@slack.org>
2023-01-11 00:04:29 -0800

    wip

--- dev/app/release.sh
+++ dev/app/release.sh
@@ -5,11 +5,12 @@ GORELEASER_CROSS_VERSION=v1.19.5
 
 # TTTTTTTTTT TODO(sqs): unskip ENTERPRISE=1 DEV_WEB_BUILDER=esbuild yarn run build-web
 
+#-e "GITHUB_TOKEN=$GITHUB_TOKEN" \
+#-e "DOCKER_USERNAME=$DOCKER_USERNAME" -e "DOCKER_PASSWORD=$DOCKER_PASSWORD" -e "DOCKER_REGISTRY=$DOCKER_REGISTRY" \
+
 exec docker run --rm --privileged \
        -v "$ROOTDIR":/go/src/github.com/sourcegraph/sourcegraph \
        -v /var/run/docker.sock:/var/run/docker.sock \
-       #-e "GITHUB_TOKEN=$GITHUB_TOKEN" \
-       #-e "DOCKER_USERNAME=$DOCKER_USERNAME" -e "DOCKER_PASSWORD=$DOCKER_PASSWORD" -e "DOCKER_REGISTRY=$DOCKER_REGISTRY" \
        -w /go/src/github.com/sourcegraph/sourcegraph \
        goreleaser/goreleaser-cross:${GORELEASER_CROSS_VERSION} \
        --config dev/app/goreleaser.yaml --rm-dist "$@"
